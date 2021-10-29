3848d8689ab0a72aeef178f34b6f2feb87d74ca9
Michael Folger <folger@zoller-usa.com>
2021-10-28 21:32:53 -0500

    updated app to add menu

---
+++ src/components/menu.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Button } from "@material-ui/core";
+
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        container justify="right" 
+        alignItems="right" 
+        direction="column"
+        id="basic-button"
+        aria-controls="basic-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        menu
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Home</MenuItem>
+        <MenuItem onClick={handleClose}>Products</MenuItem>
+        <MenuItem onClick={handleClose}>My Cart</MenuItem>
+        <MenuItem onClick={handleClose}>My Profile</MenuItem>
+      </Menu>
+    </div>
+  );
+}
