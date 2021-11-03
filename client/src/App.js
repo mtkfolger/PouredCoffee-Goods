ec81623ae1f7661296c7e16e81baaf83a63b8d03
Robert Sanders Jr <robert.sanders24@gmail.com>
2021-11-03 18:42:26 -0400

    imported login files to main page

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,46 @@ import About from './pages/about';
 import Shop from './pages/shop';
 import Cart from './pages/cart';
 import ItemDetail from './pages/itemDetail';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/home';
 import Footer from './pages/footer';
-
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences';
+import Login from './components/Login/Login';
+import useToken from './components/useToken';
 
 function App() {
-  return (
-    
-    
-    <Router>
+  
+  const {token, setToken} = useToken();
 
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/products" exact component={Shop}/>
-          <Route path="/Cart" exact component={Cart}/>
-          <Route path="/shop/:id" component={ItemDetail}/>
-        </Switch>
-        <Footer />    
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  return (
+    <Router>
+     <div className="wrapper">
+            <BrowserRouter>
+              <Switch>
+                <Route path="/dashboard">
+                  <Dashboard />
+                </Route>
+                <Route path="/preferences">
+                  <Preferences />
+                </Route>
+              </Switch>
+            </BrowserRouter>
+          </div>
+
+            <div className="App">
+              <Nav />
+              <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/products" exact component={Shop}/> 
+                <Route path="/Cart" exact component={Cart}/>
+                <Route path="/shop/:id" component={ItemDetail}/>
+              </Switch>
+              <Footer />    
      </div>
     </Router>
   );
