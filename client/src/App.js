15d6a9138938eccf320632c138b144d829d7f125
Robert Sanders Jr <robert.sanders24@gmail.com>
2021-11-05 01:34:28 -0400

    styled login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Nav from "./pages/nav";
 import About from './pages/about';
 import Shop from './pages/shop';
 import Cart from './pages/cart';
+import Login from './pages/login';
 import ItemDetail from './pages/itemDetail';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
@@ -30,6 +31,7 @@ function App() {
           <Route path="/" exact component={Home}/>
           <Route path="/about" component={About}/>
           <Route path="/products" exact component={Shop}/>
+          <Route path="/login" component={Login}/>
           <Route path="/Cart" exact component={Cart}/>
           <Route path="/shop/:id" component={ItemDetail}/>
         </Switch>
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import "../App.css";
+import React from 'react';
+import '/Users/_roban/Desktop/PouredCoffee-Goods/client/src/login.css'
 
-const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+    export default function Login() {
 
-export default Login
+        return (
+            <div className="app-area">
+                <form className="box">
+                    <div className="form-inner">
+                        <h2>Login Here</h2>
+                        <div className="form-group">
+                            <label htmlFor="name">Name:</label>
+                            <input  type="text" name="name" id="name"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email:</label>
+                            <input  type="email" name="email" id="email"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" name="password" id="password"/>
+                        </div>
+                        <input type="submit" value="SUBMIT"/>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+
 
